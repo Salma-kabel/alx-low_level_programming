d135e66cbd56bb2446d3062b47dccf59a195cec2
Salma_Salah <salma.s.kabel@gmail.com>
2023-04-25 15:28:42 -0700

    answer

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lowercase alphabet x10
+ * followed by a new line.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; i < 10; i++)
+	{
+		for (j = 97; j < 123; j++)
+		{
+			_putchar(j);
+		}
+		write (1, "\n", 1);
+	}
+}
