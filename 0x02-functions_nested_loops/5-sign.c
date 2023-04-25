aec97f6ce84672f0b17e6663cc6c672f895f3007
Salma_Salah <salma.s.kabel@gmail.com>
2023-04-25 16:13:27 -0700

    answer

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks if a number is greater or smaller or equal to 0.
+ *
+ * @n: parameter that represents the number checked.
+ *
+ * Return: 1 if greater than 0, -1 if smaller than 0, 0 if equal to 0.
+ */
+
+int print_sign(int n)
+{
+	if ( n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar(0);
+		return (0);
+	}
+}
+
