afd55f5ac1cd7babe57acd70d69caa6227396a69
Salma_Salah <salma.s.kabel@gmail.com>
2023-04-26 13:26:52 -0700

    answer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if (i < 10)
+			{
+				_putchar('0');
+				_putchar( i + 48);
+				_putchar(':');
+				if (j < 10)
+				{
+					_putchar('0');
+					_putchar(j + 48);
+				}
+				else
+				{
+					_putchar((j / 10) + 48);
+					_putchar((j % 10) + 48);
+				}
+			}
+			else
+			{
+				_putchar((i / 10) + 48);
+				_putchar((i % 10) + 48);
+				_putchar(':');
+				if (i < 10)
+				{
+					_putchar('0');
+					_putchar(j + 48);
+				}
+				else
+				{
+					_putchar((j / 10) + 48);
+					_putchar((j % 10) + 48);
+				}
+			}
+			write(1, "\n", 1);
+		}
+	}
+}
+
