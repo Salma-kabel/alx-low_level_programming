b8958beb920546e8e95b29433d46635fa029fe32
Salma_Salah <salma.s.kabel@gmail.com>
2023-04-28 14:11:42 -0700

    answer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ *
+ * @n: The integer.
+ */
+
+void print_number(int n)
+{
+	int m = n / 10;
+	int mul = 1;
+	int i, j;
+	int count = 0;
+
+	while (m != 0)
+	{
+		count++;
+		m = m / 10;
+	}
+	count++;
+	for (i = 0; i < count; i++)
+	{
+		mul = 1;
+		for (j = count - i; j > 1; j--)
+		{
+			mul = mul * 10;
+		}
+		print(count, mul, n, i);
+		n = n % mul;
+	}
+}
+
+/**
+ * print - Prints the digits.
+ * @count: number of digits.
+ * @mul: factor.
+ * @n: number.
+ * @i: counter of loop.
+ */
+void print(int count, int mul, int n, int i)
+{
+	if (i + 1 == count)
+	{
+		if (n < 0)
+		{
+			_putchar(-n + 48);
+		}
+		else
+		{
+			_putchar(n + 48);
+		}
+	}
+	else
+	{
+		if (n < 0)
+		{
+			_putchar((-n / mul) + 48);
+		}
+		else
+		{
+			_putchar((n / mul) + 48);
+		}
+	}
+}
+
