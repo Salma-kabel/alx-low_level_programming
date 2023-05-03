3c6c196e372334f91011acb0ccd61a4c1023ae93
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-03 07:33:26 -0700

    answer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer.
+ *
+ * @s: string
+ *
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+	int i, count = 0, mul = 1, b, n = 1, num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-' && s[i + 1] > 47 && s[i + 1] < 58)
+		{
+			n = -1;
+		}
+		else if (s[i] > 47 && s[i] < 58)
+		{
+			if (count == 0)
+			{
+				b = i;
+			}
+			count++;
+		}
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		num = num + s[b + i] * mul;
+		mul = mul * 10;
+	}
+	num = num * n;
+	return (num);
+}
+
