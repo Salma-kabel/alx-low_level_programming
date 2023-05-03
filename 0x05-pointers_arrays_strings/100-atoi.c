0695e85361f761daef9c8b5db2a0e36b0f35f989
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-03 07:53:25 -0700

    answer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,11 +15,11 @@ int _atoi(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == '-' && s[i + 1] > 47 && s[i + 1] < 58)
+		if (s[i] == '-')
 		{
 			n = n * -1;
 		}
-		else if (s[i] == '-' && s[i + 1] > 47 && s[i + 1] < 58)
+		else if (s[i] == '+')
 		{
 			n = n * 1;
 		}
