f8d8fd2584f20085f39f107ae6ff8318e9453a03
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 16:02:02 -0700

    answer

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,11 +11,9 @@
 char *rot13(char *str)
 {
 	int i, j;
-	char s1[] = "abcdefghijklmnopqrstuvwxyz";
-	char s2[] = "nopqrstuvwxyzabcdefghijklm";
-	char s3[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char s4[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-
+	char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; s1[j] != '\0'; j++)
@@ -23,11 +21,6 @@ char *rot13(char *str)
 			if (str[i] == s1[j])
 			{
 				str[i] = s2[j];
-				break;
-			}
-			else if (str[i] == s3[j])
-			{
-				str[i] = s4[j];
 				break;
 			}
 		}
