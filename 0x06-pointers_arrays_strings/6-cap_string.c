d7d4fa85271ddb2d0e4081ebf1c9bf97f06c40f6
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 15:06:22 -0700

    answer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,51 +1,43 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: returns length as integer;
+ * *cap_string - Capitalizes all words of a string.
+ *
+ * @str: string
+ *
+ * Return: pointer to string
  */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
-}
-
-/**
-* cap_string - function that capitalize first character of a word
-* @str: string to capitalize
-* Return: returns the capitalized string
-*/
 
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i;
 
-	while (str[++index])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (!(str[index] >= 'a') && (str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		if (str[i] > 96 && str[i] < 123)
+		{
+			if (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n')
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!')
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')')
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == '}' || str[i - 1] == ',' || str[i - 1] == '?')
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (str[i - 1] == '{')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
 	}
-
 	return (str);
 }
+
