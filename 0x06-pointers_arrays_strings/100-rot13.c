659172d58b51ede73ea9b32c7c8ba98d2dd8346c
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 15:38:20 -0700

    answer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *rot13 - Encodes a string using rot13.
+ *
+ * @str: string
+ *
+ * Return: pointer to string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char s1 = "abcdefghijklmnopqrstuvwxyz";
+	char s2 = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; s1[j] != '\0'; j++)
+		{
+			if (str[i] == s1[j])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
+
