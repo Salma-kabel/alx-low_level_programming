27bfc924ae7bac46b9c5c6165626a14b2dc1330e
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 16:03:57 -0700

    answer

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,7 +13,7 @@ char *rot13(char *str)
 	int i, j;
 	char s1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char s2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; s1[j] != '\0'; j++)
