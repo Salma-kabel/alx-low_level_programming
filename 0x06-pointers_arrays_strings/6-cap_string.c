b8cc8444756fad0b90383f275f149bfc01ff3208
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 14:25:24 -0700

    answer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,10 +14,7 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' str[i] == ','
-			|| str[i] == ';' || str[i] == '.' || str[i] == '!' ||
-			str[i] == '?' || str[i] == '"' || str[i] == '(' str[i] == ')'
-			|| str[i] == '{' || str[i] == '}')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i]== '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
