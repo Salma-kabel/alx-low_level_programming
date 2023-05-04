c1c3519ae8a7ff28bd8a65954341ff33f0491a56
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 10:45:50 -0700

    answer

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strcat - oncatenates two strings.
+ *
+ * @src: string
+ * @dest: string
+ *
+ * Return: pointer to string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		if (dest[i + 1] == '\0')
+		{
+			for (j = i + 1; src[j - i - 1] != '\0'; j++)
+			{
+				dest[j] = src[j - i - 1];
+				if (src[j - i] == '\0')
+				{
+					dest[j + 1] == '\0';
+				}
+			}
+		}
+	}
+	return (dest);
+}
+
