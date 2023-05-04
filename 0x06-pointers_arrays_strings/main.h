79b0f48a5a6d9fc863c374b173aa81132b7a59c7
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 16:19:40 -0700

    answer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print(int count, int mul, int n, int i, int k);
+
 
