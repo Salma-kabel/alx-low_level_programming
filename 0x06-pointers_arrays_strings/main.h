adc7bd741cbc8e9bec94f5f552d3253b3e8feeee
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-04 16:15:02 -0700

    answer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,7 @@ char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
+int _putchar(char c);
+
+void print(int count, int mul, int n, int i, int k);
 
