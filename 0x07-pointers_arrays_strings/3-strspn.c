f07abcd834c72b723ca711367df3d59dd0182504
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-10 14:06:27 -0700

    answer

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,14 +14,15 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int count = 0, k;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		k = count;
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			k = count;
-			if (s[j] == accept[i])
+			if (s[i] == accept[j])
 			{
 				count++;
+				break;
 			}
 		}
 		if (count == k)
