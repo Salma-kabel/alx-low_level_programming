f131d3fe82d5db565ce945ea0a445296793fdfd7
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-11 13:28:26 -0700

    answer

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,29 @@
 #include "main.h"
+#include <string.h>
 
-
+/**
+ * is_palindrome - checks if a string is palindrome
+ *
+ * @s: string
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
 
 int is_palindrome(char *s)
 {
-	
+	int i = strlen(s);
+
+	for (i = 0; i < (int) strlen(s) / 2; i++)
+	{
+		if (s[i] == s[strlen(s) - (i + 1)])
+		{
+			continue;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
