e76f86d411e8b05c508f1de6e553a130a33b1071
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-11 15:15:02 -0700

    answer

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,17 +22,13 @@ int wildcmp(char *s1, char *s2)
 			return (0);
 		}
 	}
-	else if (s1[0] == '\0')
-	{
-		return (1);
-	}
 	else if (s1[0] == s2[0])
 	{
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (s1[0] != '\0' && wildcmp(s1 + 1, s2 + 1));
 	}
 	else if (s2[0] == '*')
 	{
-		return (wildcmp(s1, s2 + 1) || (wildcmp(s1 + 1, s2)));
+		return (wildcmp(s1, s2 + 1) || (s1[0] != '\0' && wildcmp(s1 + 1, s2)));
 	}
 	else
 	{
