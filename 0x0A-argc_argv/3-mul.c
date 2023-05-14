a6f4be7ec2205cf234b3e8ed9a63e3d8f3a788b6
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-14 14:08:07 -0700

    answer

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -44,13 +44,9 @@ int main(int argc, char *argv[])
 			n2 = n2 + (argv[2][i] - 48) * mul;
 		}
 		if (argv[1][0] == '-')
-		{
-			n1 = - n1;
-		}
-		else if (argv[2][0] == '-')
-		{
-			n2 = - n2;
-		}
+			n1 = -n1;
+		if (argv[2][0] == '-')
+			n2 = -n2;
 		printf("%d\n", n1 * n2);
 		return (0);
 	}
