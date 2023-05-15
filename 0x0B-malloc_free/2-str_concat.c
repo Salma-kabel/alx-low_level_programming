6121c88fa3f2ce34d1b81c482f9bfe606fe74bcb
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-15 14:47:19 -0700

    answer

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,16 +15,31 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *arr;
 
-	arr = malloc(sizeof(strlen(s1) + strlen(s2)) + 1);
+	if (s1 == NULL && s2 == NULL)
+	{
+		return (NULL);
+	}
+	else if (s1 == NULL)
+	{
+		arr = malloc(sizeof(strlen(s2)) + 1);
+	}
+	else if (s2 == NULL)
+	{
+		arr = malloc(sizeof(strlen(s1)) + 1);
+	}
+	else
+	{
+		arr = malloc(sizeof(strlen(s1) + strlen(s2)) + 1);
+	}
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0' && s1 != NULL; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		arr[i] = s1[i];
 	}
-	for (j = 0; s2[j] != '\0' && s2 != NULL; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 	{
 		arr[i] = s2[j];
 		i++;
