e2afd34fef7465362fd24a8e28410bb4ca262258
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-18 13:32:03 -0700

    answer

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,16 +12,21 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int i;
 	void *arr;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
-	arr = malloc(nmemb * size + 1);
+	arr = malloc(nmemb * size);
 	if (arr == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < nmemb * size; i++)
+	{
+		arr[i] = 0;
 	}
 	return (arr);
 }
