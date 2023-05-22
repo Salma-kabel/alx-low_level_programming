384ce9bc7d1752dfd849738fa4f4672f5b4f4e09
Salma_Salah <salma.s.kabel@gmail.com>
2023-05-22 13:25:18 -0700

    answer

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+*struct dog - data type
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
