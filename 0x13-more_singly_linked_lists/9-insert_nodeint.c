0513c4e2cd7d48b8cb6503814ed9f3f92154c0d0
Salma_Salah <salma.s.kabel@gmail.com>
2023-06-07 14:35:16 -0700

    answer

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = (*head)->next;
 		idx--;
 		if (*head == NULL)
-			break;
+			return (NULL);
 	}
 	if (idx == 0 && *head != NULL)
 	{
