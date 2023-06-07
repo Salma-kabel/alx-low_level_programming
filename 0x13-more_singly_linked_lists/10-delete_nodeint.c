24cf4ef41c699c0d649752f31975b126f13b69c5
Salma_Salah <salma.s.kabel@gmail.com>
2023-06-07 15:08:22 -0700

    answer

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *p, *p2;
+	listint_t *p;
 
 	p = *head;
 	if (p == NULL)
@@ -24,13 +24,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	index--;
 	while (index != 0)
 	{
-		if (p->next == NULL)
+		if ((*head)->next == NULL)
 			return (-1);
-		p = p->next;
+		(*head) = (*head)->next;
 		index--;
 	}
-	p2 = p->next;
-	p->next = p2->next;
-	free(p2);
+	p = (*head)->next;
+	(*head)->next = p->next;
+	free(p);
 	return (1);
 }
