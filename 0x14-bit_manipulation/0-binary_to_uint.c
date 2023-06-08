14faf4270c42bc82936986c72dbc9978c93d2f28
Salma_Salah <salma.s.kabel@gmail.com>
2023-06-08 06:17:07 -0700

    answer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,7 +10,7 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, j, num = 0, p = 1;
 
-	if (b == '\0')
+	if (b == NULL || strlen(b) == 0)
 		return (0);
 
 	for (i = 0; i < strlen(b); i++)
