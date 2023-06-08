5fff74cfcf5abf6cba394dcdf88b1f3b7445e391
Salma_Salah <salma.s.kabel@gmail.com>
2023-06-08 07:29:53 -0700

    answer

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,5 +8,8 @@
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
 
 int get_endianness(void);
+
+#endif
