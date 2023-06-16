fe96d22db24510312c81ddcfc323be44dbb74e8b
Salma_Salah <salma.s.kabel@gmail.com>
2023-06-16 12:59:27 -0700

    answer

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,7 +5,7 @@
  * @filename: file to append to
  * @text_content: text to append
  * Return: 1 if success , -1 otherwise
- */ 
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
