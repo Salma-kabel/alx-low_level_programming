1b7e2a58079f1730669d57bbaafc7157ee11a2e0
Salma_Salah <salma.s.kabel@gmail.com>
2023-07-06 13:31:14 -0700

    answer

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,16 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+	if (n <= 0)
+		return;
+
+	print_binary(n >> 1);
+	if ((n & 1) == 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
