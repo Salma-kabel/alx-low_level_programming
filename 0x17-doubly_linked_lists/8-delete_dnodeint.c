c9cffa4d06a100f9d0a4e7ef42c59c8aed464174
Salma_Salah <salma.s.kabel@gmail.com>
2023-08-23 12:15:47 -0700

    answer2

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,6 +22,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	ptr2->next = ptr->next;
 	ptr->next->prev = ptr->prev;
-	
+	free(ptr->next);
+	free(ptr->prev);
+	free(ptr);
 	return (1);
 }
