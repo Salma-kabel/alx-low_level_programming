5671d40abc2c3d52878b6f587dca43b90fe05c5f
Salma_Salah <salma.s.kabel@gmail.com>
2023-08-23 12:01:28 -0700

    answer2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		new = add_dnodeint(h, n);
 	}
 	else
 	{
@@ -39,7 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		else
 		{
-			add_dnodeint_end(h, n);
+			new = add_dnodeint_end(h, n);
 		}
 	}
 	return (new);
