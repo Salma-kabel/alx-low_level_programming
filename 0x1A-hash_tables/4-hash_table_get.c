df14eb7924d028d617a8898699c327a035257540
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-09 06:31:35 -0700

    answer

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,11 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_get - gets a value associated with a key.
+ * @ht: pointer to hash table
+ * key: key to get value for
+ * Return: value for the key or null if failed
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
