1e2f5e71d5c8730400ee89ee49e186c4ae9d0b3e
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-09 07:37:16 -0700

    answer

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,6 +1,9 @@
 #include "hash_tables.h"
 
-
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: pointer to hash table
+ */
 
 void hash_table_print(const hash_table_t *ht)
 {
@@ -13,16 +16,15 @@ void hash_table_print(const hash_table_t *ht)
 	for (i = 0; i < ht->size; i++)
 	{
 		ptr = ht->array[i];
-		
 		while (ptr != NULL)
 		{
 			if (j == 0)
 			{
-				printf("'%s': '%s'",ptr->key, ptr->value);
+				printf("'%s': '%s'", ptr->key, ptr->value);
 				j = 1;
 			}
 			else
-				printf(", '%s': '%s'",ptr->key, ptr->value);
+				printf(", '%s': '%s'", ptr->key, ptr->value);
 			ptr = ptr->next;
 		}
 	}
