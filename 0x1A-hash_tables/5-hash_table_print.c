976309a2f6145dffd59b8a357efe86bbb2cb45c9
Salma_Salah <salma.s.kabel@gmail.com>
2023-09-09 07:35:00 -0700

    answer

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *ptr;
+	unsigned long int i, j = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		ptr = ht->array[i];
+		
+		while (ptr != NULL)
+		{
+			if (j == 0)
+			{
+				printf("'%s': '%s'",ptr->key, ptr->value);
+				j = 1;
+			}
+			else
+				printf(", '%s': '%s'",ptr->key, ptr->value);
+			ptr = ptr->next;
+		}
+	}
+	printf("}\n");
+}
