77dfb7193fa47db6d84e38683e2be260da079867
Salma_Salah <salma.s.kabel@gmail.com>
2023-10-25 13:07:55 -0700

    answer

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """Grid cells are connected horizontally/vertically (not diagonally)."""
+    perimeter = 0
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            if grid[i][j] == 1:
+                if (i - 1) >= 0 and grid[i - 1][j] == 0:
+                    perimeter += 1
+                if (i + 1) < len(grid[0]) and grid[i + 1][j] == 0:
+                    perimeter += 1
+                if (j - 1) >= 0 and grid[i][j - 1] == 0:
+                    perimeter += 1
+                if (j + 1) < len(grid) and grid[i][j + 1] == 0:
+                    perimeter += 1
+    return(perimeter)
