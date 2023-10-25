24fb507335633398d843c9c1522d770fc46b979a
Salma_Salah <salma.s.kabel@gmail.com>
2023-10-25 13:13:44 -0700

    answer

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,8 +5,8 @@
 def island_perimeter(grid):
     """Grid cells are connected horizontally/vertically (not diagonally)."""
     perimeter = 0
-    for i in range(len(grid[0])):
-        for j in range(len(grid)):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 if (i - 1) >= 0 and grid[i - 1][j] == 0:
                     perimeter += 1
