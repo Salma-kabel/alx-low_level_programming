cefafcf6f807c9bd802eae4ab692bb504acb471f
Salma_Salah <salma.s.kabel@gmail.com>
2023-12-13 06:39:51 -0800

    answer

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -33,10 +33,9 @@ int binary_search(int *array, size_t size, int value)
 	else if (array[num] < value)
 	{
 		num += 1;
-		i = binary_search(array + num, size - num, value);
+		i = num + binary_search(array + num, size - num, value);
 		if (i == -1)
 			return (-1);
-		return (i + num);
 	}
 	return (i);
 }
