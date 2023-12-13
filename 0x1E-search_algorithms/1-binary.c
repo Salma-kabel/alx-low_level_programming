34865240ec7b43230150c65e874dcd15b9ff60ac
Salma_Salah <salma.s.kabel@gmail.com>
2023-12-13 06:17:45 -0800

    answer

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,7 +11,7 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int num, i, j;
+	int num = ((size - 1) / 2), i, j;
 
 	if (array == NULL || size < 1)
 		return (-1);
@@ -24,7 +24,6 @@ int binary_search(int *array, size_t size, int value)
 		else
 			printf("\n");
 	}
-	num = (size - 1) / 2;
 	if (array[num] == value)
 		return (num);
 	else if (size <= 1)
