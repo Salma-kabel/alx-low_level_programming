197a8d92ed6a6744e26eb79ec4414ee5fc54a5a8
Salma_Salah <salma.s.kabel@gmail.com>
2023-12-13 05:22:10 -0800

    answer

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,3 +1,6 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,3 +8,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+
+#endif
